680bad694e8772638e4bdfe91eee4ef372bf6406
Janis Celms <janiscelms@Janiss-MacBook-Pro.local>
2016-01-06 06:31:12 +0200

    Automated commit at 2016-01-06 04:31:12 UTC by middleman-deploy 1.0.0

--- config.rb
+++ config.rb
@@ -26,11 +26,11 @@ end
 
 
 activate :deploy do |deploy|
-  deploy.deploy_method = :git
+  deploy.method = :git
   deploy.build_before = true
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
+    deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
